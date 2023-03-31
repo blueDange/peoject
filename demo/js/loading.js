cd71ef9ca635c284a941cc31de747f216b882c2e
blueDange <672074694@qq.com>
2023-03-31 16:13:22 +0800

    先隐藏大盒子,等页面加载动画转一会在加载主体内容

---
+++ demo/js/loading.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function () {
+  // Hide the loading animation
+
+  document.querySelector('.loading').style.display = 'none'
+
+  // Show the page content
+  document.querySelector('.background').style.display = 'block'
+})
